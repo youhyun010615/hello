95f7efd5e54e3d13dda2964dae6a034ba7bac672
Ksejoong <133006726+Ksejoong@users.noreply.github.com>
2023-05-09 20:23:18 +0900

    Add files via upload
    enemy1~9공격 및 이미지 기본 틀

---
+++ shooting/src/shooting/Main.java
@@ -0,0 +1,12 @@
+package shooting;
+
+public class Main {
+	public static final int SCREEN_WIDTH = 1280;
+	public static final int SCREEN_HEIGHT = 1280;
+
+	public static void main(String[] args) {
+	
+
+	}
+
+}
